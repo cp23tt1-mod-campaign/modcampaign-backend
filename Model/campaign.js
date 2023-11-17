6e5fe67dbea1d8650654f21d1ad3f5c8bd2ec282
JirasinQ <jirasin4826@gmail.com>
2023-11-17 11:24:47 +0700

    :heavy_minus_sign: Remove comment

--- Model/campaign.js
+++ Model/campaign.js
@@ -8,14 +8,5 @@ class CampaignDAO {
   async readCampaignById(data) {
     return dataMapper(data[0], campaignDetailDTO);
   }
-  // async createCampaign(campaignData) {
-  //   await db("campaign").insert(campaignData);
-  // }
-  // async updateCampaign(campaignId, campaignData) {
-  //   await db("campaign").where("campaignId", campaignId).update(campaignData);
-  // }
-  // async deleteCampaign(campaignId) {
-  //   await db("campaign").where("campaignId", campaignId).del();
-  // }
 }
 module.exports = new CampaignDAO();
